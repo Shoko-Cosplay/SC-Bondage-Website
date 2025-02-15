9173b4f72979913de6aeab46d888b7362d650f73
Serreau Jovann <website@shoko-cosplay.fr>
2025-02-15 15:14:32 +0100

    Prepare test env

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,16 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+;
+
+return (new PhpCsFixer\Config())
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
+    ->setRules([
+        '@Symfony' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'yoda_style' => false,
+    ])
+    ->setFinder($finder)
+    ;
